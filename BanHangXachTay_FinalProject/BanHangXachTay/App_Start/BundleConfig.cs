68b0b0a4b5ffa86a41d648f0b6e35adf0918dedc
phuocdt123 <tranquangphuoc0@gmail.com>
2019-04-18 02:55:28 +0700

    Quản Lý Sản Phẩm Sửa CSS

--- BanHangXachTay_FinalProject/BanHangXachTay/App_Start/BundleConfig.cs
+++ BanHangXachTay_FinalProject/BanHangXachTay/App_Start/BundleConfig.cs
@@ -23,9 +23,7 @@ namespace BanHangXachTay
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css/bootstrap").Include("~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
         }
     }
 }
