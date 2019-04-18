23e7431c3a2dc06df80d05d8ace6f0fd2f075379
ngvanloi <nguyenloi.itse@gmail.com>
2019-04-18 11:18:24 +0700

    Them giao dien contact cho admin va user

---
+++ BanHangXachTay_FinalProject/BanHangXachTay/Controllers/ContactController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanHangXachTay.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Admin()
+        {
+            return View();
+        }
+	}
+}
