2219e9ca5b8a1e0ea463061cc38ae69f628d8341
quangnguyenne1411@gmail.com <quangnguyenne1411@gmail.com>
2019-04-24 14:53:38 +0700

    Nguyên commit!
    Nguyên commit!

---
+++ BanHangXachTay_FinalProject/BanHangXachTay/Controllers/YourCartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanHangXachTay.Controllers
+{
+    public class YourCartController : Controller
+    {
+        // GET: YourCart
+        public ActionResult YourCartViews()
+        {
+            return View();
+        }
+    }
+}
