43566121ffb19aa9d1895a2b5f09cb37ff58162b
quangnguyenne1411@gmail.com <quangnguyenne1411@gmail.com>
2019-05-02 00:29:09 +0700

    Nguyeen!
    456

---
+++ BanHangXachTay_FinalProject/BanHangXachTay.Tests/Controllers/YourCartControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+using BanHangXachTay.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BanHangXachTay.Tests.Controllers
+{
+    [TestClass]
+    public class YourCartControllerTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var controller = new YourCartController();
+
+            var result = controller.YourCartViews() as ViewResult;
+
+            Assert.IsNotNull(result);
+
+        }
+    }
+}
