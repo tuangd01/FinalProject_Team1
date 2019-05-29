33f2e66b5307be6c08013cffd9df96ddcd48e8de
maianhkiet21199 <kiet.t175095@vanlanguni.vn>
2019-05-29 18:38:43 +0700

    update

---
+++ BanHangXachTay_FinalProject/BanHangXachTay/Controllers/KH_TaoThongTinLienHeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BanHangXachTay.Models;
+
+namespace BanHangXachTay.Controllers
+{
+    public class KH_TaoThongTinLienHeController : Controller
+    {
+        private CsK23T2aEntities1 db = new CsK23T2aEntities1();
+
+        // GET: KH_TaoThongTinLienHe
+        public ActionResult Index()
+        {
+            return View(db.Contacts.ToList());
+        }
+
+        // GET: KH_TaoThongTinLienHe/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // GET: KH_TaoThongTinLienHe/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: KH_TaoThongTinLienHe/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idContact,chuDe,tieuDe,noiDung,hoTen,email,sdt,thoiGianGui,trangThai,traLoi,thoigianTraLoi,ghiChu")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contact);
+        }
+
+        // GET: KH_TaoThongTinLienHe/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: KH_TaoThongTinLienHe/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idContact,chuDe,tieuDe,noiDung,hoTen,email,sdt,thoiGianGui,trangThai,traLoi,thoigianTraLoi,ghiChu")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contact).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contact);
+        }
+
+        // GET: KH_TaoThongTinLienHe/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: KH_TaoThongTinLienHe/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            db.Contacts.Remove(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
